0a39d23b4d29bbab6e61a95a2d41b68abbe1cbe1
jeffvarghese5 <jeff.varghese5@gmail.com>
2022-09-14 10:03:41 +1000

    Extra Run is removed

--- Ellink/Ellink/Program.cs
+++ Ellink/Ellink/Program.cs
@@ -23,4 +23,4 @@ app.UseAuthorization();//Hello second person
 app.MapRazorPages();//Hello11111111ddddd
 
 app.Run();//hello1111 - This is local branch
-app.Run();//hello1111 - This is main branch
+
